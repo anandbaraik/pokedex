7f2eb31290dbbb0f74d53a11884038688ca54c89
anandbaraik <anandbaraik2014@gmail.com>
2022-04-01 22:41:29 +0530

    Searchbox implemented

--- src/components/Pokedex/Pokedex.tsx
+++ src/components/Pokedex/Pokedex.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PokeSearchResult from '../PokeSearchResult/PokeSearchResult'
+import SearchBox from '../SearchBox/SearchBox'
 import "./Pokedex.css"
 export default function Pokedex() {
   return (
     <div className='pokedex-container'>
       <div className='pokelist-container'>
+      <SearchBox/>
         <p>Pokeon list</p>
       </div>
       <div className='pokedex-search-result-container'>
---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./SearchBox.css";
+const SearchBox = () => {
+  return (
+    <input type="search" className='search-box' placeholder='Search pokemon'/>
+  )
+}
+
+export default SearchBox
