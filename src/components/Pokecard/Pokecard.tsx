116e0f4ce891f2c2e829b4fc801930600a391810
anandbaraik <anandbaraik2014@gmail.com>
2022-04-01 23:10:01 +0530

    pokecard component added

---
+++ src/components/Pokecard/Pokecard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Pokecard.css"
+const Pokecard = () => {
+  return (
+    <div className='pokecard'>
+        {/* Image */}
+        <p>
+            Test
+        </p>
+    </div>
+  )
+}
+
+export default Pokecard
